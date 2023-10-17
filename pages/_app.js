684561bc7c85d31c091cb6ac0eec3f8eb451c08e
Yifan Zhang <yifan2019@icloud.com>
2023-10-17 10:04:54 +0300

    draft

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,128 @@
-import '@styles/globals.css'
+import { GeistProvider, CssBaseline, Themes } from '@geist-ui/core'
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const futuristic = Themes.createFromLight({
+  type: 'futuristic',
+  palette: {
+    accents_1: '#121212', // Deep Space Black
+    accents_2: '#1EFF00', // Neon Green
+    accents_5: '#8B00FF', // Galactic Purple
+    accents_6: '#007BFF', // Electric Blue
+    accents_7: '#C0C0C0', // Cyber Silver
+    background: '#121212', // Deep Space Black for background
+    foreground: '#C0C0C0', // Cyber Silver for text
+    selection: '#007BFF', // Electric Blue for selections
+    secondary: '#1EFF00', // Neon Green
+  },
+  expressiveness: {
+    dropdownBoxShadow: '0 0 0 1px #007BFF',
+    shadowSmall: '0 0 0 1px #007BFF',
+    shadowMedium: '0 0 2px 0 #007BFF',
+    shadowLarge: '0 0 4px 0 #007BFF',
+    portalOpacity: 0.75,
+  },
+  font: {
+    sans: "'Share Tech Mono', monospace",
+  },
+})
+
+const futuristic2 = Themes.createFromLight({
+  type: 'futuristic2',
+  palette: {
+    accents_1: '#151515', // Adjusted Deep Space Black for better contrast
+    accents_2: '#33FF00', // Brighter Neon Green for emphasis
+    accents_5: '#9B00FF', // Adjusted Galactic Purple
+    accents_6: '#008BFF', // Richer Electric Blue
+    accents_7: '#D0D0D0', // Lighter Cyber Silver for readability
+    background: '#151515', // Deep Space Black for background
+    foreground: '#D0D0D0', // Cyber Silver for text
+    selection: '#008BFF', // Electric Blue for selections
+    secondary: '#33FF00', // Neon Green
+    code: '#FFFF00', // Bright Yellow for code blocks, ensures high readability
+  },
+  expressiveness: {
+    dropdownBoxShadow: '0px 0px 8px 1px #007BFF', // Glowing effect
+    shadowSmall: '0px 0px 0px 1px #007BFF', // Subtle glow
+    shadowMedium: '0px 0px 8px 2px #007BFF', // Medium glowing effect
+    shadowLarge: '0px 0px 16px 2px #007BFF', // Strong glowing effect
+    portalOpacity: 0.9, // Slightly more visible portals
+    linkStyle: 'none', // Remove default link style for a cleaner look
+    linkHoverStyle: 'none', // Remove default link hover style for consistency
+  },
+  font: {
+    sans: "'Orbitron', sans-serif", // Changed to Orbitron for a more futuristic feel
+    mono: "'Share Tech Mono', monospace", // Kept Share Tech Mono for monospace requirements
+  },
+  layout: {
+    gap: '1.5rem', // More space between elements
+    gapNegative: '-1.5rem',
+    gapHalf: '.75rem',
+    gapHalfNegative: '-.75rem',
+    gapQuarter: '.375rem',
+    gapQuarterNegative: '-.375rem',
+    pageMargin: '2rem', // More breathing room on the sides
+    pageWidth: '1000px', // Set a max-width for better readability on larger screens
+    pageWidthWithMargin: '1040px',
+    breakpointMobile: '720px', // Adjusted for better mobile responsiveness
+    breakpointTablet: '960px', // Adjusted for better tablet responsiveness
+    breakpointDesktop: '1280px', // Standard desktop breakpoint
+  }
+})
+
+
+const technology = Themes.createFromLight({
+  type: 'technology', // name of the theme
+  palette: {
+    // Using colors that represent technology, trust, and innovation
+    foreground: '#000000',
+    background: '#ffffff',
+    success: '#00ab6b', // A green that is often associated with growth, harmony, freshness, safety, and environment.
+    error: '#ff6a6a', // A softer shade of red to indicate errors without being too alarming.
+    warning: '#f0e68c', // A gentle yellow that indicates caution without causing anxiety.
+    link: '#0366d6', // A shade of blue that's often associated with trust and stability.
+    secondary: '#333333',
+    code: '#f81ce5',
+    border: '#eaeaea',
+    tableDivider: '#f8f8f8',
+    selectionBackground: '#f81ce5',
+    selectionForeground: '#ffffff',
+    accent: '#0070f3', // A bright color for focus areas, using a slightly different blue to differentiate and provide a modern, energetic vibe.
+    cyan: '#79ffe1', // An innovative and eye-catching color for interactive elements.
+    purple: '#f81ce5', // An energetic color that can be used for highlighting actions or information.
+    violet: '#7928ca', // Represents luxury and offers a deeper contrast.
+    alert: '#ff0080',
+  },
+  expressiveness: {
+    dropdownBoxShadow: '0 0 0 1px #333',
+    shadowSmall: '0 0 0 1px #333',
+    shadowMedium: '0 0 2px 0 #333',
+    shadowLarge: '0 0 15px 0 #333',
+    portalOpacity: 0.75,
+  },
+  layout: {
+    gap: '16pt',
+    gapNegative: '-16pt',
+    gapHalf: '8pt',
+    gapHalfNegative: '-8pt',
+    gapQuarter: '4pt',
+    gapQuarterNegative: '-4pt',
+    pageMargin: '0pt',
+    pageWidth: '1000pt',
+    pageWidthWithMargin: '1000pt',
+    radius: '5pt',
+  },
+  font: {
+    sans: '"Inter", sans-serif', // A clean, neutral, modern sans-serif font. Make sure you're loading this font in your app if it's not already.
+    mono: '"Fira Code", monospace', // A font designed for code, which could match your technology-oriented style.
+  },
+})
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <GeistProvider themes={[futuristic, technology, futuristic2]} themeType='futuristic2'>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </GeistProvider>
+  )
 }
 
-export default Application
+export default App
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,102 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { useTheme } from '@geist-ui/core'
+import { Image, Display, Divider, Text, Description, Grid, Spacer, Page, Card, Button } from '@geist-ui/core'
+import { Calendar, MapPin, Globe, Home } from '@geist-ui/icons'
 
-export default function Home() {
+export default function HomePage() {
+  const { palette } = useTheme()
+
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Join us at QCRI Generative AI Hackathon!</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet"/>
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+      <Page align="center">
+        <Card height="500px" style={{
+          backgroundImage: "url('/main-background.png')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}>
+          <Card.Content style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', height: "100%"}}>
+            <Text h1 type="default" align="center" style={{ color: palette.accents_2, background: palette.background}}>Generative AI Hackathon</Text>
+            <Text span align="center" style={{ color: palette.accents_2, background: palette.background}}>December 7-9, 2023</Text>
+            <Text span align="center" style={{ color: palette.accents_2, background: palette.background}}>Multi-purpose Room</Text>
+            <Text span align="center" style={{ color: palette.accents_2, background: palette.background}}>QCRI, Doha</Text>
+          </Card.Content>
+        </Card>
 
-      <Footer />
+        <Spacer h={8} />
+
+          <Text h2 align="center" style={{ color: palette.accents_2}}> What is Generative AI? </Text>
+          <Text blockquote align="center">
+          Generative AI is an emerging technology that has been growing in popularity due to its tremendous potential for a wide range of applications. At its core, it is an artificial intelligence system designed to generate content, such as text, images, or music, for a variety of purposes. One notable example of such technology is ChatGPT, which has the ability to create human-like responses in the context of a conversation. The advancement of generative AI has enabled more efficient solutions for numerous industries, such as customer service, content creation, and entertainment. Its continued development promises to reshape how we interact with technology, leading to more seamless and efficient AI-driven experiences, and unlocking new opportunities for personalized, engaging, and creative interactions.
+          </Text>
+          <Button type="success">Register Now!</Button>
+
+        <Spacer h={8} />
+
+            <Text h2 style={{ color: palette.accents_2}}>Event Details</Text>
+          <Spacer y={2} />
+          <Grid.Container gap={8} justify="center">
+            <Grid xs={12}>
+              <MapPin size={64} color="green" />
+              <Spacer x={0.5} />
+              <Description title={<Text h4>In-Person</Text>} content={<Text p>This event will be held at QCRI</Text>} />
+            </Grid>
+            <Grid xs={12}>
+              <Calendar size={64} color="green"/>
+              <Spacer x={0.5} />
+              <Description title={<Text h4>Dec 7-9, 2023</Text>} content={<Text p>December 7 - pre-event, December 8-9 - Hackathon</Text>} />
+            </Grid>
+            <Grid xs={12}>
+              <Globe size={64} color="green"/>
+              <Spacer x={0.5} />
+              <Description title={<Text h4>Theme</Text>} content={<Text p>Use Generative AI for Education, Social Good, and many other </Text>} />
+            </Grid>
+            <Grid xs={12}>
+              <Home size={64} color="green"/>
+              <Spacer x={0.5} />
+              <Description title={<Text h4>Theme</Text>} content={<Text p>Use Generative AI for Education, Social Good, and many other </Text>} />
+            </Grid>
+          </Grid.Container>
+
+        <Spacer h={8} />
+
+            <Text h2 type="secondary">Sponsors</Text>
+          <Spacer y={2} />
+          <Grid.Container gap={2} justify="center">
+            <Grid xs={6}>
+              <Card shadow width="100%">
+                <Text h4>Sponsor Name</Text>
+                <Image src="/hackathon-logo.png" width="300px" height="180px" />
+              </Card>
+            </Grid>
+            <Grid xs={6}>
+              <Card shadow width="100%">
+                <Text h4 type="warning">Sponsor Name</Text>
+                <Image src="/hackathon-logo.png" width="300px" height="180px" />
+              </Card>
+            </Grid>
+            <Grid xs={6}>
+              <Card shadow width="100%">
+                <Text h4 type="link">Sponsor Name</Text>
+                <Image src="/hackathon-logo.png" width="300px" height="180px" />
+              </Card>
+            </Grid>
+          </Grid.Container>
+
+        <Spacer h={8} />
+
+        <Page.Footer>
+          <Text type="secondary" align="center" small>
+            Copyright &copy; 2023, Qatar Computing Research Institute
+          </Text>
+        </Page.Footer>
+      </Page>
     </div>
   )
 }
